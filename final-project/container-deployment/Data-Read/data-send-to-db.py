da5b3ca7f58e5e8688ebcbb544f1fb32c454a687
Camren Hall <camren.hall@vanderbilt.edu>
2021-12-13 06:50:20 -0600

    wrote twitter stream

---
+++ final-project/container-deployment/Data-Read/data-send-to-db.py
@@ -0,0 +1,42 @@
+
+# Author: Camren Hall, Safet Hoxha, Luke Garrett
+# CS4287-5287: Principles of Cloud Computing, Vanderbilt University
+#
+# Created: Sept 22, 2020
+#
+# Purpose:
+#    Use Kafka consumer API to stream data to CouchDB
+
+import json
+import couchdb
+
+ip_couchdb = "129.114.27.39"
+
+# acquire couchdb server
+user = "admin"
+password = "cloudhgh"
+conn_string  = "http://{}:{}@{}:30010".format(user, password, ip_couchdb)
+print(conn_string)
+couch = couchdb.Server(conn_string)
+dbname = "mr-documents"
+
+if dbname in couch:
+    db = couch[dbname]
+else:
+    db = couch.create(dbname)
+
+document = open('starwars.txt','r')
+
+doc_id, doc_rev = db.save({'name': 'Demo', 'text': document.read()})
+
+document.close()
+
+for docid in db.view('_all_docs'):
+    print(str(db[docid['id']]['text']))
+    
+
+
+
+
+
+
---
+++ final-project/container-deployment/consumer.py
@@ -0,0 +1,31 @@
+
+# Author: Camren Hall, Safet Hoxha, Luke Garrett
+# CS4287-5287: Principles of Cloud Computing, Vanderbilt University
+#
+# Created: Sept 22, 2020
+#
+# Purpose:
+#    Use Kafka consumer API to stream data to CouchDB
+
+import json
+from kafka import KafkaConsumer  # consumer of events
+
+# ip = "ec2-3-137-205-212.us-east-2.compute.amazonaws.com" #niu
+# acquire consumer
+
+consumer = KafkaConsumer(bootstrap_servers="129.114.24.229:9092", value_deserializer=lambda v: json.loads(v).decode('utf-8'))
+# subscribe to topic
+consumer.subscribe (topics=["tweetdata"])
+
+# we keep reading and printing
+for msg in enumerate(consumer):
+    print (msg)
+
+consumer.close ()
+    
+
+
+
+
+
+
