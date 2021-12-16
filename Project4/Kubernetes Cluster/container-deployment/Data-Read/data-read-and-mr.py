b96736def26f5b91338d4c0e1c36629cc29ebb0f
Chameleon Cloud User <cc@teamhgh-ansible-chameleon-008.novalocal>
2021-12-16 15:24:59 +0000

    removed prod

--- Project4/Kubernetes Cluster/container-deployment/Data-Read/data-read-and-mr.py
+++ Project4/Kubernetes Cluster/container-deployment/Data-Read/data-read-and-mr.py
@@ -1,14 +1,12 @@
 import json
 from operator import add
-from kafka import KafkaProducer
 from pyspark.sql import SparkSession
 from pyspark.sql.types import *
 from decimal import Decimal
 import couchdb
+import time
 
-
-producer = KafkaProducer (bootstrap_servers="129.114.24.229:30000", value_serializer=lambda v: json.dumps(v).encode('ascii'))
-producer.config
+curr = time.perf_counter()
 
 def func(x):
     if x[0][3] == 0:
@@ -36,7 +34,7 @@ if __name__ == "__main__":
     conn_string  = "http://{}:{}@{}:30010".format(user, password, ip_couchdb)
     print(conn_string)
     couch = couchdb.Server(conn_string)
-    dbname = "energy-data"
+    dbname = "hghdata"
 
     if dbname in couch:
         db = couch[dbname]
@@ -54,31 +52,30 @@ if __name__ == "__main__":
     df = spark.createDataFrame(emptyRDD, schema)
     
 
-    i = 0
-    for docid in db.view('_all_docs'):
-        data = [(Decimal(db[docid['id']]['value']), int(db[docid['id']]['property']),int(db[docid['id']]['plug_id']),int(db[docid['id']]['household_id']),int(db[docid['id']]['house_id']))]
-        tmp = spark.createDataFrame(data, schema)
-        df = df.union(tmp)
-        print(i)
-        i = i + 1
+    rowsTotal = db.view('_all_docs', include_docs=True)
+    data = [(Decimal(rows['doc']['value']), int(rows['doc']['property']),int(rows['doc']['plug_id']),int(rows['doc']['household_id']),int(rows['doc']['house_id'])) for rows in rowsTotal]
+    df = spark.createDataFrame(data, schema)
+    curr2 = time.perf_counter()
+    print('Downloading Complete. Time elapsed: ', (curr2 - curr))
     finalRDD = df.rdd
     mappedRDD = finalRDD.map(lambda x: ((x[4],x[3],x[2],x[1]),x[0]))
     aTuple = (0,0)
     mappedRDD = mappedRDD.aggregateByKey(aTuple, lambda a,b: (a[0] + b, a[1] + 1), lambda a,b: (a[0] + b[0], a[1] + b[1]))
     mappedRDD = mappedRDD.mapValues(lambda v: v[0]/v[1])
     mappedRDD = mappedRDD.map(lambda x: func(x)).reduceByKey(lambda x,y: (x[0]+y[0], x[1]+y[1])).collect()
-    
+    curr = time.perf_counter()
+    print('Calculations Complete. Time elapsed: ', (curr - curr2))
     if 'results' in couch:
         db2 = couch['results']
     else:
         db2 = couch.create('results')
     
+    f = open('results.txt', 'w')
+    f.write('house_id\t\thousehold_id\t\tplug_id\t\taverage_work\t\taverage_load\n')
     for (ident, vals) in mappedRDD:
-            dict = {'house_id' : ident[0], 'household_id' : ident[1],'plug_id' : ident[2], 'average_work' : vals[0], 'average_load' : vals[1]}
-            db2.save(dict)
+    	f.write(str(ident[0])+'\t\t'+str(ident[1])+'\t\t'+str(ident[2])+'\t\t'+str(vals[0])+'\t\t'+str(vals[1])+'\n')
+            
+    curr2 = time.perf_counter()
+    print('Uploading Complete. Time elapsed: ', (curr2 - curr))
     
-    
-
-    producer.close()
     spark.stop()
-
